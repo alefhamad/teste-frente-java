9a2975d0d816315c9e67eec4455df7f8c2187ceb
Alef Hamad <alefhamad@gmail.com>
2022-12-24 11:36:17 -0300

    Fixing folder Structure

--- temp/temp/PacoteRepo.java
+++
@@ -1,9 +0,0 @@
-package br.com.frentecorretora.fakeatm.repos;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.frentecorretora.fakeatm.models.PacoteModel;
-
-public interface PacoteRepo extends CrudRepository<PacoteModel, Long>{
-    
-}
--- temp/temp/TransacaoRepo.java
+++
@@ -1,9 +0,0 @@
-package br.com.frentecorretora.fakeatm.repos;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.frentecorretora.fakeatm.models.TransacaoModel;
-
-public interface TransacaoRepo extends CrudRepository<TransacaoModel, Long>{
-    
-}
